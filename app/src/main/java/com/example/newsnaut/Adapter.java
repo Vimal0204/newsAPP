1ef95894b494f93ea988a93f8762931d2ccfe71d
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2023-06-04 22:40:15 +0530

    Update Adapter.java

--- app/src/main/java/com/example/newsnaut/Adapter.java
+++ app/src/main/java/com/example/newsnaut/Adapter.java
@@ -15,7 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
-
+// this is an example
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     Context context;
